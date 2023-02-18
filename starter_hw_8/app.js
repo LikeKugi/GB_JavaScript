f013a1dfdc9735e65ca2b3b9c3cb48bdcfb80b52
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-02-18 18:01:58 +0300

    started div block with cart items

--- starter_hw_8/app.js
+++ starter_hw_8/app.js
@@ -29,9 +29,12 @@ filterSizeWrap.addEventListener("click", function () {
   filterSizes.classList.toggle("hidden");
 });
 
+// -----------------------------------------------------------
+
 const supplies = {};
 const featuredItems = document.querySelector(".featuredItems");
 const cartQty = document.querySelector("#cartQty");
+const cartIconEl = document.querySelector(".cartIconWrap");
 
 featuredItems.addEventListener("click", (event) => {
   if (!event.target.classList.contains("featuredBtnAdd")) {
@@ -55,7 +58,7 @@ featuredItems.addEventListener("click", (event) => {
     };
   }
 
-  console.log(supplies);
+  // console.log(supplies);
   countSupplies();
 });
 
@@ -64,10 +67,45 @@ function countSupplies() {
   let qty = 0;
   for (const id in supplies) {
     const supply = supplies[id];
-    console.log(supply);
+    // console.log(supply);
     qty += supply.quantity;
     sum += supply.price * supply.quantity;
   }
-  console.log(`sum = ${sum}; qty = ${qty}`);
+  // console.log(`sum = ${sum}; qty = ${qty}`);
+  cartQty.style.visibility = qty ? "visible" : "hidden";
   cartQty.textContent = qty;
+}
+
+const boxTemplate = document.querySelector("#cartBox");
+const rowTemplate = document.querySelector("#cartRow");
+const wrapper = document.querySelector(".rightHeader");
+
+cartIconEl.addEventListener("click", () => {
+  if (Object.keys(supplies).length && !wrapper.querySelector(".cartItems")) {
+    console.log(supplies);
+    makeCart();
+  }
+});
+
+function makeCart() {
+  const box = boxTemplate.content.cloneNode(true);
+  let TOTAL = 0;
+  for (const ID in supplies) {
+    const supply = supplies[ID];
+    console.log(supply);
+    const clone = rowTemplate.content.cloneNode(true);
+    const name = clone.querySelector(".name");
+    name.textContent = supply.name;
+    const quantity = clone.querySelector(".quantity");
+    quantity.textContent = supply.quantity;
+    const price = clone.querySelector(".price");
+    price.textContent = "$" + supply.price;
+    const total = clone.querySelector(".total");
+    total.textContent = "$" + +supply.price * +supply.quantity;
+    TOTAL += +supply.price * +supply.quantity;
+    console.log(clone);
+    box.querySelector("tbody").append(clone);
+  }
+  box.querySelector("#total").textContent = TOTAL;
+  wrapper.append(box);
 }
