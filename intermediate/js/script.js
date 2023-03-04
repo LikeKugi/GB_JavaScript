6e654ef4b6e9cce1f1574a2c2afb590b10c39a15
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-04 23:18:02 +0300

    lesson 5 vue

--- intermediate/js/script.js
+++ intermediate/js/script.js
@@ -4,6 +4,54 @@ const goods = [
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
 ];
+const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const GET_GOODS_ITEMS = `${BASE_URL}/catalogData.json`;
+const GET_GOODS_BASKET_ITEMS = `${BASE_URL}/getBasket.json`;
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        goods: [],
+        searchLine: '',
+        isVisibleCart: false,
+    },
+    methods: {
+        makeGETRequest(url, callback) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.send();
+
+            xhr.onload = function () {
+                if (xhr.readyState === 4) {
+                    callback(JSON.parse(xhr.response));
+                }
+            }
+        },
+        toggleCart() {
+            this.isVisibleCart = !this.isVisibleCart;
+        }
+    },
+    mounted() {
+        this.makeGETRequest(GET_GOODS_ITEMS, (goods) => {
+            this.goods = goods;
+        });
+    },
+    computed: {
+        calculatePrice() {
+            return this.goods.reduce((a, {price}) => a + price, 0)
+        },
+        filteredGoods() {
+            return this.goods.filter((item) => {
+                const query = new RegExp(this.searchLine, 'i');
+                return query.test(item.product_name);
+            })
+        }
+    }
+});
+
+
+
+
 
 class GoodsItem {
     _price;
@@ -37,7 +85,7 @@ class GoodsList {
     }
 }
 
-const supplies = new GoodsList;
-supplies.fetchGoods();
-supplies.render();
-console.log(supplies.totalPrice());
+// const supplies = new GoodsList;
+// supplies.fetchGoods();
+// supplies.render();
+// console.log(supplies.totalPrice());
